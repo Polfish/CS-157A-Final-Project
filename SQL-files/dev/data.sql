a2b81a13fc09c1145e2f77e41e8c02690dc86ccc
Polfish <45519040+Polfish@users.noreply.github.com>
2021-12-08 23:48:10 -0800

    Revised schema for dev and release; Wrote example data for dev database
    Added auto_increment to the primary keys

--- SQL-files/dev/data.sql
+++ SQL-files/dev/data.sql
@@ -1,20 +1,45 @@
 -- This is example data to test on
 -- User table
-INSERT INTO user
-VALUES ();
-
--- Account table
-INSERT INTO account
-VALUES ();
+INSERT INTO user (username, password)
+VALUES 
+('rainy', 'password'),
+('bob-ffa', '02938*$71'),
+('e', '93jf9345-');
 
 -- Budget table
-INSERT INTO budget
-VALUES ();
+INSERT INTO budget (user_id, budget_name, amount, startDate, endDate)
+VALUES 
+((SELECT user_id FROM user WHERE username = 'rainy'), 'Save on water', 3, 2021128, 99991231),
+((SELECT user_id FROM user WHERE username = 'rainy'), 'Paying off Student Loan', 107382, 2021121, 21871121),
+((SELECT user_id FROM user WHERE username = 'bob-ffa'), 'Grocery shopping', 10, 20210501, 20210508),
+((SELECT user_id FROM user WHERE username = 'e'), 'Anything entertainment', 800, 20200801, 20200831),
+((SELECT user_id FROM user WHERE username = 'bob-ffa'), 'Computer parts', 1000000, 2021128, 2021129);
+
+-- Account table
+INSERT INTO account (account_name, user_id, balance)
+VALUES 
+('Bank of America', (SELECT user_id FROM user WHERE username = 'bob-ffa'), 58173.78),
+('Netflix', (SELECT user_id FROM user WHERE username = 'bob-ffa'), 0),
+('Netflix', (SELECT user_id FROM user WHERE username = 'rainy'), -12.99),
+('Wells Fargo', (SELECT user_id FROM user WHERE username = 'e'), 78167342.82),
+('Student Loan', (SELECT user_id FROM user WHERE username = 'rainy'), -107382),
+('Chase', (SELECT user_id FROM user WHERE username = 'rainy'), 3);
 
 -- Category table
-INSERT INTO category
-VALUES ();
+INSERT INTO category (category_name)
+VALUES 
+('Entertainment'),
+('Essentials'),
+('Insurance'),
+('Savings'),
+('Debts');
 
 -- Transaction table
-INSERT INTO transaction
-VALUES ();
+INSERT INTO transaction (category_id, account_id, date, notes, amount)
+VALUES 
+((SELECT category_id FROM category WHERE category_name = 'Entertainment') 
+(SELECT account_id FROM account WHERE account_name = 'Netflix' AND balance = 0), 20190314, 'I want to watch something fun!', 1000),
+((SELECT category_id FROM category WHERE category_name = 'Essentials') 
+(SELECT account_id FROM account WHERE account_name = 'Bank of America'), 99991231, 'water', 1),
+((SELECT category_id FROM category WHERE category_name = 'Debts') 
+(SELECT account_id FROM account WHERE account_name = 'Student Loan'), 20211121, 'Starting to pay of my student loan!', 9187);
--- SQL-files/dev/dev-schema.sql
+++ SQL-files/dev/dev-schema.sql
@@ -9,14 +9,14 @@ USE finance_tracker_dev;
 
 -- Create tables
 CREATE TABLE user (
-    user_id INT,
+    user_id INT AUTO_INCREMENT,
     username VARCHAR(128),
     password VARCHAR(128),
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE budget (
-    budget_id INT,
+    budget_id INT AUTO_INCREMENT,
     user_id INT,
     budget_name VARCHAR(128),
     amount DOUBLE,
@@ -27,7 +27,7 @@ CREATE TABLE budget (
 );
 
 CREATE TABLE account (
-    account_id INT,
+    account_id INT AUTO_INCREMENT,
     account_name VARCHAR(128),
     user_id INT,
     balance DOUBLE,
@@ -36,13 +36,13 @@ CREATE TABLE account (
 );
 
 CREATE TABLE category (
-    category_id INT,
+    category_id INT AUTO_INCREMENT,
     category_name VARCHAR(128),
     PRIMARY KEY (category_id)
 );
 
 CREATE TABLE transaction (
-    transaction_id INT,
+    transaction_id INT AUTO_INCREMENT,
     category_id INT,
     account_id INT,
     date INT,
@@ -52,3 +52,13 @@ CREATE TABLE transaction (
     FOREIGN KEY (category_id) REFERENCES category(category_id),
     FOREIGN KEY (account_id) REFERENCES account(account_id)
 );
+
+-- Creating user views
+-- CREATE VIEW AS MonthlySpendingReport
+-- ;
+
+-- CREATE VIEW AS UserBudgetReport
+-- ;
+
+-- CREATE VIEW AS NetWorthBudget
+-- ;
